9aa9e5499c0f868358c2b51cd31d9a7a63df18b7
Mathias Arzberger <develop@pdir.de>
2019-12-17 17:15:53 +0100

    move classes; add logo and default fe_page

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 // Insert the zero one theme category
-array_insert($GLOBALS['TL_CTE'], 1, array('zeroOneTheme' => array()));
+array_insert($GLOBALS['TL_CTE'], 1, ['zeroOneTheme' => []]);
 
 /**
  * Add content elements
@@ -11,21 +11,38 @@ array_insert($GLOBALS['TL_CTE'], 1, array('zeroOneTheme' => array()));
 /**
  * Available tags for Zero One Theme
  */
-$GLOBALS['tl_config']['theme_tags'] = array(
+$GLOBALS['tl_config']['theme_tags'] = [
     '-',
-    'NATURE01/01',
-    'NATURE01/02',
-    'NATURE01/03',
-    'NATURE01/04',
-    'NATURE01/05',
-    'NATURE02/01',
-    'NATURE02/02',
-    'NATURE02/03',
-    'NATURE02/04',
-    'NATURE02/05',
-    'NATURE03/01',
-    'NATURE03/02',
-    'NATURE03/03',
-    'NATURE03/04',
-    'NATURE03/05',
-);
+    'ZeroOne01/01',
+    'ZeroOne01/02',
+    'ZeroOne01/03',
+    'ZeroOne01/04',
+    'ZeroOne01/05',
+    'ZeroOne02/01',
+    'ZeroOne02/02',
+    'ZeroOne02/03',
+    'ZeroOne02/04',
+    'ZeroOne02/05',
+    'ZeroOne03/01',
+    'ZeroOne03/02',
+    'ZeroOne03/03',
+    'ZeroOne03/04',
+    'ZeroOne03/05',
+];
+
+/**
+ * Load default styles for every page
+ */
+if($GLOBALS['ZERO_ONE_STYLES'])
+    $GLOBALS['ZERO_ONE_STYLES'] = [];
+
+$GLOBALS['ZERO_ONE_STYLES'][] = 'variables';
+$GLOBALS['ZERO_ONE_STYLES'][] = 'mixins';
+$GLOBALS['ZERO_ONE_STYLES'][] = 'normalize';
+$GLOBALS['ZERO_ONE_STYLES'][] = 'base';
+$GLOBALS['ZERO_ONE_STYLES'][] = 'layout';
+// $GLOBALS['ZERO_ONE_STYLES'][] = '';
+// $GLOBALS['ZERO_ONE_STYLES'][] = '';
+// $GLOBALS['ZERO_ONE_STYLES'][] = '';
+// $GLOBALS['ZERO_ONE_STYLES'][] = '';
+// $GLOBALS['ZERO_ONE_STYLES'][] = '';
