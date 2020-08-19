8e51d700c18f144aaf14800c9bb391980b97bca3
Philipp Seibt <seibt@pdir.de>
2020-08-19 16:47:19 +0200

    fix bug: version is hidden if you click to synchronize theme files

--- src/Module/ZeroOneThemeSetup.php
+++ src/Module/ZeroOneThemeSetup.php
@@ -22,11 +22,13 @@ class ZeroOneThemeSetup extends \BackendModule
                 $this->getFiles($path);
                 $this->getSqlFiles($path = TL_ROOT . "/vendor/contao-themes-net/zero-one-theme-bundle/src/templates");
                 $this->Template->message = true;
+                $this->Template->version = ZeroOneThemeSetup::VERSION;
                 break;
             case 'truncateTlFiles':
                 $this->import('Database');
                 $this->Database->prepare("TRUNCATE tl_files")->execute();
                 $this->Template->messageTruncate = true;
+                $this->Template->version = ZeroOneThemeSetup::VERSION;
                 break;
             default:
                 $this->Template->version = ZeroOneThemeSetup::VERSION;
