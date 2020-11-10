a2db2383ac2d3fbc0c43acd387509109c88b4da3
Philipp Seibt <seibt@pdir.de>
2020-11-10 17:45:03 +0100

    fix scss paths

--- src/Module/ZeroOneThemeSetup.php
+++ src/Module/ZeroOneThemeSetup.php
@@ -4,7 +4,7 @@ namespace ContaoThemesNet\ZeroOneThemeBundle\Module;
 
 class ZeroOneThemeSetup extends \BackendModule
 {
-    const VERSION = '1.2.8';
+    const VERSION = '1.2.9';
 
     protected $strTemplate = 'be_zeroonetheme_setup';
 
