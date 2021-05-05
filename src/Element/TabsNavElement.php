53dbdf39655976909e9aed471b34f3ba78fb4e23
Mathias Arzberger <develop@pdir.de>
2021-05-05 16:47:39 +0200

    code styling

--- src/Element/TabsNavElement.php
+++ src/Element/TabsNavElement.php
@@ -19,14 +19,17 @@ class TabsNavElement extends \ContentElement
 		$limit = \count($items) - 1;
 		for ($i=0, $c=\count($items); $i<$c; $i++)
 		{
-		    if(!isset($items[$i]['default'])) $items[$i]['default'] = '';
-			$arrItems[] = array
-			(
+		    if (!isset($items[$i]['default']))
+            {
+                $items[$i]['default'] = '';
+            }
+            
+			$arrItems[] = [
 				'class' => (($i == 0) ? 'first' : (($i == $limit) ? 'last' : '')),
 				'content' => $items[$i]['label'],
 				'value' => $items[$i]['value'],
                 'default' => $items[$i]['default']
-			);
+			];
 		}
 		$this->Template->items = $arrItems;
     }
