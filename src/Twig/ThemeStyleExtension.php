aa9d778851721a9d685e7cd6b9612026e9d0fcef
Mathias Arzberger <arzberger@pdir.de>
2022-10-27 16:06:58 +0200

    add theme style extension

---
+++ src/Twig/ThemeStyleExtension.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * 0.1 theme for Contao Open Source CMS
+ *
+ * Copyright (C) 2022 pdir / digital agentur // pdir GmbH
+ *
+ * @package    contao-themes-net/zero-one-theme-bundle
+ * @link       https://github.com/contao-themes-net/zero-one-theme-bundle
+ * @license    pdir contao theme licence
+ * @author     Mathias Arzberger <develop@pdir.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ContaoThemesNet\ZeroOneThemeBundle\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class ThemeStyleExtension extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('addToThemeStyles', [$this, 'addStylesToGlobal']),
+        ];
+    }
+
+    public function addStylesToGlobal(array $styles): void
+    {
+        $GLOBALS['ZERO_ONE_STYLES'] = array_merge($GLOBALS['ZERO_ONE_STYLES'], $styles);
+    }
+}
