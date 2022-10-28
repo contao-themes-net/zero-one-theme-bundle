c8e74585f75b29c9f22860057eca77d1716544ba
Mathias Arzberger <arzberger@pdir.de>
2022-10-28 12:47:25 +0200

    add file twig extension

---
+++ src/Twig/FileExtension.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * 0.1 theme for Contao Open Source CMS
+ *
+ * Copyright (C) 2022 pdir / digital agentur // pdir GmbH
+ *
+ * @package    contao-themes-net/zero-one-theme-bundle
+ * @link       https://github.com/contao-themes-net/zero-one-theme-bundle
+ * @license    pdir contao theme licence
+ * @author     Mathias Arzberger <develop@pdir.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ContaoThemesNet\ZeroOneThemeBundle\Twig;
+
+use Contao\CoreBundle\Framework\ContaoFramework;
+use Contao\FilesModel;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class FileExtension extends AbstractExtension
+{
+    public function __construct(private readonly ContaoFramework $framework)
+    {
+    }
+
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('file', [$this, 'getFile']),
+        ];
+    }
+
+    public function getFile(string $uuid, $attribute = null): string|int|array|float
+    {
+        $filesAdapter = $this->framework->getAdapter(FilesModel::class);
+        $filesModel = $filesAdapter->findByUuid($uuid);
+
+        if (null === $filesModel) {
+            return '';
+        }
+
+        return match ($attribute) {
+            'pid' => $filesModel->pid,
+            'tstamp' => $filesModel->tstamp,
+            'type' => $filesModel->type,
+            'path' => $filesModel->path,
+            'extension' => $filesModel->extension,
+            'hash' => $filesModel->hash,
+            'lastModified' => $filesModel->lastModified,
+            'name' => $filesModel->name,
+            'importantPartX' => $filesModel->importantPartX,
+            'importantPartY' => $filesModel->importantPartY,
+            'importantPartWidth' => $filesModel->importantPartWidth,
+            'importantPartHeight' => $filesModel->importantPartHeight,
+            'absolutePath' => $filesModel->getAbsolutePath(),
+            'metaFields' => $filesModel->getMetaFields() ?? [],
+            'metadata' => $filesModel->getMetadata() ?? [],
+            default => $filesModel->id,
+        };
+    }
+}
