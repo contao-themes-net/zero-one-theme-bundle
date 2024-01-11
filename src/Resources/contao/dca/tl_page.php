d07365fe4cc3f6eeabbc89cea5e20715a6c03137
Philipp Seibt <seibt@pdir.de>
2024-01-11 15:54:13 +0100

    add image size for megamenu image

--- src/Resources/contao/dca/tl_page.php
+++ src/Resources/contao/dca/tl_page.php
@@ -2,10 +2,12 @@
 
 use Contao\Config;
 use Contao\CoreBundle\DataContainer\PaletteManipulator;
+use Contao\System;
 
 PaletteManipulator::create()
     ->addLegend('megamenu_legend', 'meta_legend', PaletteManipulator::POSITION_AFTER)
     ->addField('megamenuImage', 'megamenu_legend', PaletteManipulator::POSITION_APPEND)
+    ->addField('megamenuImageSize', 'megamenu_legend', PaletteManipulator::POSITION_APPEND)
     ->applyToPalette('regular', 'tl_page')
 ;
 
@@ -14,4 +16,13 @@ $GLOBALS['TL_DCA']['tl_page']['fields']['megamenuImage'] = [
     'exclude' => true,
     'eval' => ['fieldType' => 'radio', 'files' => true, 'filesOnly' => true, 'extensions' => Config::get('validImageTypes')],
     'sql' => 'blob NULL',
+];
+
+$GLOBALS['TL_DCA']['tl_page']['fields']['megamenuImageSize'] = [
+    'label' => &$GLOBALS['TL_LANG']['MSC']['imgSize'],
+    'exclude' => true,
+    'inputType' => 'imageSize',
+    'eval' => ['rgxp' => 'natural', 'includeBlankOption' => true, 'nospace' => true, 'helpwizard' => true, 'tl_class' => 'w50'],
+    'options_callback' => static fn () => System::getContainer()->get('contao.image.image_sizes')->getOptionsForUser(BackendUser::getInstance()),
+    'sql' => 'TEXT null',
 ];
